8601a7908fdeb3ca14aedbb1ce5af720cfa654a6
Ricardo-Cs <rcsilva2405@gmail.com>
2025-10-08 09:00:41 -0500

    Atualizações de nomenclatura

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import { env } from './config/env';
+import { errorHandler } from './middlewares/errorHandler';
+import routes from './routes';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", routes);
+
+app.use((_req, res) => {
+    res.status(404).json({ error: "Rota inexistente" });
+});
+
+app.use(errorHandler);
+
+app.listen(env.PORT, () => {
+    console.log(`Rodando na porta ${env.PORT}`);
+});
---
+++ src/routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import categoryRouter from "./modules/categories/category.routes";
+import productRouter from "./modules/products/product.routes";
+import userRouter from "./modules/users/user.routes";
+import authRouter from "./modules/auth/auth.routes";
+import swaggerRouter from "./config/swagger";
+
+const router = Router();
+
+router.use("/categories", categoryRouter);
+router.use("/products", productRouter);
+router.use("/users", userRouter);
+router.use("/auth", authRouter);
+router.use("/", swaggerRouter);
+
+export default router;

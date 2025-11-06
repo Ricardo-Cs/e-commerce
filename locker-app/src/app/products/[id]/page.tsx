239f296c8563fa6ee4a759e920c5173e642e8f22
Ricardo-Cs <rcsilva2405@gmail.com>
2025-11-06 15:44:17 -0500

    Adiciona teste de tela de produto

--- locker-app/src/app/products/[id]/page.tsx
+++ locker-app/src/app/products/[id]/page.tsx
@@ -1,17 +1,14 @@
 interface PageProps {
-    params: { id: string };
+    params: Promise<{ id: string }>
 }
 
 export default async function ProductPage({ params }: PageProps) {
-    const { id } = await (params);
+    const { id } = await params
 
-    // Aqui você buscaria os dados do produto pelo ID
-    // Por exemplo, com uma API ou banco de dados
-    const product = {
-        name: `Produto ${id}`,
-        description: `Descrição do produto ${id}`,
-        price: 99.9,
-    };
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${id}`, {
+        cache: "no-store",
+    })
+    const product = await res.json()
 
     return (
         <div>
@@ -19,5 +16,5 @@ export default async function ProductPage({ params }: PageProps) {
             <p>{product.description}</p>
             <strong>R$ {product.price}</strong>
         </div>
-    );
+    )
 }

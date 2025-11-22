5c5b605407e734cd99f27f5cd2062f24ba91e95f
Ricardo-Cs <rcsilva2405@gmail.com>
2025-11-22 13:49:08 -0500

    teste

--- locker-app/src/app/products/[id]/page.tsx
+++ locker-app/src/app/products/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import { Key } from "react";
 import { Breadcrumbs } from "../../../components/layout/breadCumbs";
+import Image from "next/image";
 
 interface PageProps {
   params: Promise<{ id: string }>;
@@ -22,6 +25,10 @@ export default async function ProductPage({ params }: PageProps) {
         <h1>{product.name}</h1>
         <p>{product.description}</p>
         <strong>R$ {product.price}</strong>
+
+        {product.images?.map((img: { id: Key | null | undefined; url: string | StaticImport }) => (
+          <Image key={img.id} src={img.url} alt={product.name} width={500} height={500} className="object-cover" />
+        ))}
       </div>
     </div>
   );
